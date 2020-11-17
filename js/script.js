0972d7a2f09d9a158ded36586074bb6a0b38144b
PowerNether <64768544+PowerNether@users.noreply.github.com>
2020-11-17 18:03:48 +0500

    fix

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbwFC6TIcPRTqOVInU_hy-7TDsCRQ5kGW7M5L0GUEp8Tc-H6w0lC/exec";
-const form = document.forms["submit-to-google-sheet"];
+var form = $("form#test-form"),
+  url =
+    "https://script.google.com/macros/s/AKfycbwFC6TIcPRTqOVInU_hy-7TDsCRQ5kGW7M5L0GUEp8Tc-H6w0lC/exec";
 
-form.addEventListener("submit", (e) => {
+var $form = (form.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+      if (o[this.name]) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || "");
+      } else {
+        o[this.name] = this.value || "";
+      }
+    });
+    return o;
+  })
+$("#submit-form").on("click", function (e) {
   e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
-    .catch((error) => console.error("Error!", error.message));
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form,
+  });
 });
